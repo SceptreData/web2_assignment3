4b546002451e87a8237db5c2e84b7dccb49d8a2c
David Bergeron <david.f.bergeron@gmail.com>
2019-04-08 23:27:04 -0600

    pushing work on ass3, mostly finished just have to optimize.

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,31 @@
-const checkBoxes = document.querySelectorAll(".checkbox");
+window.addEventListener("DOMContentLoaded", function(event) {
+  const menu = document.querySelector("#mobile-menu");
+  const menuButton = document.querySelector("#menu-icon");
 
-for (const checkBox of checkBoxes) {
-  checkBox.addEventListener("click", e => {
-    let [check] = checkBox.children;
-    if (checkBox.classList.contains("isChecked")) {
-      check.style.opacity = 0;
-      checkBox.classList.remove("isChecked");
+  let isOpen = false;
+
+  menuButton.addEventListener("click", () => {
+    console.log("click");
+    if (isOpen) {
+      TweenMax.to(menu, 0.6, { left: "-100vw", ease: Power3.easeIn });
     } else {
-      check.style.opacity = "1.0";
-      checkBox.classList.add("isChecked");
+      TweenMax.to(menu, 0.4, { left: 0, ease: Power3.easeOut });
     }
+    isOpen = !isOpen;
   });
-}
+
+  const checkBoxes = document.querySelectorAll(".checkbox");
+
+  for (const checkBox of checkBoxes) {
+    checkBox.addEventListener("click", e => {
+      let [check] = checkBox.children;
+      if (checkBox.classList.contains("isChecked")) {
+        check.style.opacity = 0;
+        checkBox.classList.remove("isChecked");
+      } else {
+        check.style.opacity = "1.0";
+        checkBox.classList.add("isChecked");
+      }
+    });
+  }
+});
